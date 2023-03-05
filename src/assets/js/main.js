6c7e370c30d86534e78cd01a92f7a41bc14acb25
suika05 <dev.suika05@gmail.com>
2023-03-05 09:33:16 +0900

    add

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,19 +1,54 @@
 "use strict"
+const $body = document.querySelector('body');
 
 let $taskValue = document.querySelectorAll('[data-input]')[0];
 let $taskBtn = document.querySelectorAll('[data-btn]')[0];
 let $taskList = document.querySelectorAll('[data-list]')[0];
+let taskNum = 0;
 
-$taskBtn.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  let task = $taskValue.value;
+let $completeCheckbox;
 
-  let _html = `
-  <div class="p-todo__main-list-item">
-    ${task}
-  </div>`;
+function addTask(){
+  $taskBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    let task = $taskValue.value;
+  
+    let taskLength = document.querySelectorAll('[data-task]').length;
+  
+    if(!(task.length === 0)) {
+      taskNum = document.querySelectorAll('.p-todo__main-list-item').length + 1;
+  
+      let _html = `
+      <div class="p-todo__main-list-item" data-task>
+        ${task}
+        <label for="complete_checked-${taskNum}">
+        <input type="checkbox" class="js-complete-check" id="complete_checked-${taskNum}" name="complete">
+          完了
+        </label>
+      </div>`;
+    
+      $taskList.insertAdjacentHTML('afterbegin',_html);
+    
+      $taskValue.value = '';
+  
+      $completeCheckbox = document.querySelectorAll('.js-complete-check');
+  
+    }
+  });
+};
 
-  $taskList.insertAdjacentHTML('afterbegin',_html);
+function completeTask() {
+  document.addEventListener('click',function(evt){
+    if (evt.target.className === 'js-complete-check') {
+      let length = $completeCheckbox.length;
+  
+      let targetParent = evt.target.closest('[data-task]')
+      console.log(targetParent);
+    } 
+  });
+};
 
-  $taskValue.value = '';
+window.addEventListener('load',function(){
+  addTask();
+  completeTask();
 })
